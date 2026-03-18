12a8df3f9862f96c43c4b9b2ada4515f9247cbf5
NhatBarbie001 <LDnhat001@gmail.com>
2026-03-18 17:08:45 +0700

    ...

--- loraclip/loralib/layers.py
+++ loraclip/loralib/layers.py
@@ -478,12 +478,12 @@ class MultiheadAttention(nn.Module):
         self.coef_k = nn.ParameterList([
         nn.Parameter(torch.randn(self.n_frq, generator=g_cuda, device=self.device))
         for _ in range(n_tasks)
-        ])
+        ], requires_grad=True)
 
         self.coef_v = nn.ParameterList([
         nn.Parameter(torch.randn(self.n_frq, generator=g_cuda, device=self.device))
         for _ in range(n_tasks)
-        ])
+        ], requires_grad=True)
 
         self.indices = [
         self.select_pos(t, self.embed_dim, generator=g_cpu).to(self.device)
